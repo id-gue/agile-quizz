f2c8fe678d9f55106fdf8ab51b26afaf92e4744b
Ines Dorian Gütt <gue@danke.fish>
2018-05-14 21:47:31 +0200

    added js

--- quiz.js
+++ quiz.js
@@ -1,1 +1,14 @@
-questions.js
+const quizContainer = document.getElementById('question');
+const resultsContainer = document.getElementById('results');
+const submitButton = document.getElementById('submit');
+
+function buildQuiz(){};
+
+//display questions and possibilities to answer
+buildQuiz();
+
+//import quesstions from json file
+
+var json = require('quiz.json');
+
+console.log(['json', json]);
